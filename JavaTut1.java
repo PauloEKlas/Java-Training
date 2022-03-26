7f991d8cc3c8b63ce4666310758b572ccc9fd491
PauloEKlas <72419438+PauloEKlas@users.noreply.github.com>
2022-03-26 22:59:46 +0000

    bom dia

---
+++ JavaTut1.java
@@ -0,0 +1,29 @@
+public class JavaTut1 {
+
+	public static void main(String[] args) {
+		// System.out.println("Madlibs!");
+		
+		// System.out.println("O meu ____ fez uma grande ____ no meio da ____!");
+		
+		// String primeiro = "irmão";
+		// String segundo = "porcaria";
+		// String terceiro = "sala";
+		
+		// System.out.println("O meu " + primeiro + " fez uma grande " + segundo + " no meio da " + terceiro + "!");
+
+        // System.out.println("Choose your pokemon:");
+
+        // try (Scanner scan = new Scanner(System.in)) {
+		// 	String pokemon1 = scan.nextLine();
+
+			// System.out.println("Chosen pokemon: " + pokemon1);
+			for(int i=0;i<5;i++){
+				System.out.print("\n");
+				for(int j=0;j<5;j++){
+					System.out.println("Very nice!");
+					System.out.print("\n");
+				}
+			}
+		// }
+	}
+}
---
+++ School.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class School {
+    public static void main(String [] args){
+
+        // Teacher verifier
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+
+        String professor = myObj.nextLine();
+
+        // System.out.println(professor == "Paulo");
+
+        Boolean wtv = (professor.equals("Paulo"));
+        if(!wtv){    
+            System.out.println("Sorry but only Paulo is allowed in this program.");
+            System.exit(0);
+        }
+ 
+        // End of teacher verifier
+        // Grading time
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the grade:");
+        int grade = scan.nextInt();
+
+        if(grade > 100 || grade < 0){
+            while(grade > 100 || grade < 0){
+                System.out.println("This kind of grade doesn't exist, please input a proper grade:");
+                grade = scan.nextInt();
+            }
+        }
+        scan.close();
+        if(grade <= 100 && grade >= 90){
+            System.out.println("Nota: Muito bom.");
+        }else if(grade >=80 && grade <= 90){
+            System.out.println("Nota: Bom.");
+        }else if(grade >=50 && grade <= 75){
+            System.out.println("Nota: Suficiente.");
+        }else if(grade >=20 && grade <= 49){
+            System.out.println("Nota: Insuficiente.");
+        }else if(grade >=0 && grade <= 20){
+            System.out.println("Nota: Fraco.");
+        }
+        // End of grading time
+    }
+}
